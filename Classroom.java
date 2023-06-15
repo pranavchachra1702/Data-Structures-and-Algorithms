c7f77712bbbc8ede2669ed94bf6e57f3314b6b58
Pranav <pranavchachra1702@gmail.com>
2023-06-15 21:12:54 +0530

    Insert and Search in a trie

---
+++ Classroom.java
@@ -0,0 +1,53 @@
+public class Classroom{
+    static class Node{
+        Node[] children;
+        boolean eow;
+
+        public Node(){
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+    static Node root = new Node();
+    public static void insert(String word){
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+               curr.children[idx] = new Node();
+            }
+            
+            if(i == word.length() - 1)
+            curr.children[idx].eow = true;
+
+            curr = curr.children[idx];
+        }
+    }
+    public static boolean search(String word){
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            
+            if(curr.children[idx] == null)
+            return false;
+
+            if(i == word.length() - 1  && curr.children[idx].eow == false)
+            return false;
+
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any"};
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        System.out.println(search("their"));
+        System.out.println(search("thor"));
+        System.out.println(search("an"));
+    }
+}
