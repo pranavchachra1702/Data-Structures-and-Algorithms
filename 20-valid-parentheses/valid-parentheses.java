54b2cc92c39f86f5fac7342a8156a2a8dec24b6c
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2024-03-25 13:28:07 +0530

    Time: 49 ms (5.06%) | Memory: 44.8 MB (5.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -17,5 +17,26 @@ class Solution
             return s.isEmpty();
 
         }
+
+        //Using Stack:
+        /*
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(char c : s.toCharArray())
+        {
+            if(c == '(')
+            stack.push(')');
+
+            else if(c == '{')
+            stack.push('}');
+
+            else if(c == '[')
+            stack.push(']');
+
+            else if(stack.isEmpty() || stack.pop() != c)
+            return false;
+        }
+
+        return stack.isEmpty();*/
     }
 }
