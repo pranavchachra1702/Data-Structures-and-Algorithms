f850ba1eaa73d33a6fb38232bc8a7f74575083af
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2024-03-26 09:51:05 +0530

    Time: 1 ms (98.00%) | Memory: 55.5 MB (25.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,6 +3,8 @@ class Solution
     public int firstMissingPositive(int[] nums) 
     {
         int n = nums.length;
+        /* Logic 1:
+        TC = O(n), SC = O(n)
         boolean[] seen = new boolean[n + 1];
 
         for(int num : nums)
@@ -15,6 +17,29 @@ class Solution
         {
             if(!seen[i])
             return i;
+        }
+        */
+
+        // Logic 2: 
+        // TC: O(n), SC: O(1) 
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] <= 0 || nums[i] > n)
+            nums[i] = n + 1;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            int num = Math.abs(nums[i]);
+
+            if(num <= n)
+            nums[num - 1] = -Math.abs(nums[num - 1]);
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > 0)
+            return i + 1;
         }
         return n + 1;
     }
