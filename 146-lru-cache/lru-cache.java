1e369c74c1fbdc62264402d336584c52bb39dda8
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2024-03-27 11:23:32 +0530

    Update lru-cache.java

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,3 +1,23 @@
+public class LRUCache extends LinkedHashMap<Integer, Integer>
+{
+        private final int maxCap;
+        
+        public LRUCache(int capacity)
+        {
+            super(capacity, 1.0f, true);
+            maxCap = capacity;
+        }
+
+        protected boolean removeEldestEntry(Map.Entry eldest) 
+        {
+            return size() > maxCap;
+        }
+
+        public int get(int key) 
+        {
+            return super.getOrDefault(key, -1);
+        }
+}
 /*class LRUCache 
 {
     class Node
@@ -85,24 +105,9 @@
     }
 }
 */
-    public class LRUCache extends LinkedHashMap<Integer, Integer>
-    {
-        private final int maxCap;
-        
-        public LRUCache(int capacity) {
-            super(capacity, 1.0f, true);
-            maxCap = capacity;
-        }
-
-        protected boolean removeEldestEntry(Map.Entry eldest) {
-            return size() > maxCap;
-        }
-
-        public int get(int key) {
-            return super.getOrDefault(key, -1);
-        }
-    }
+    
 /**
+Refer this also: https://www.geeksforgeeks.org/design-a-data-structure-for-lru-cache/
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
  * int param_1 = obj.get(key);
