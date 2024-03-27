04393dc36361ec48cbf1d4663f031ce91deff3c4
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2024-03-27 11:11:25 +0530

    Time: 41 ms (94.84%) | Memory: 111.3 MB (87.22%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,4 +1,4 @@
-class LRUCache 
+/*class LRUCache 
 {
     class Node
     {
@@ -84,7 +84,24 @@ class LRUCache
         nextt.prev = prevv;
     }
 }
+*/
+    public class LRUCache extends LinkedHashMap<Integer, Integer>
+    {
+        private final int maxCap;
+        
+        public LRUCache(int capacity) {
+            super(capacity, 1.0f, true);
+            maxCap = capacity;
+        }
 
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            return size() > maxCap;
+        }
+
+        public int get(int key) {
+            return super.getOrDefault(key, -1);
+        }
+    }
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
